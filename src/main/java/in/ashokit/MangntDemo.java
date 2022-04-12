47335e7c710cf4903eb4cf4a8c8783dd4ead34d0
ajay-chandra <ajaychandra098@gmail.com>
2022-04-12 21:17:58 +0530

    initial commit with k variable

--- src/main/java/in/ashokit/MangntDemo.java
+++ src/main/java/in/ashokit/MangntDemo.java
@@ -3,7 +3,7 @@ package in.ashokit;
 public class MangntDemo {
 
 	public static void main(String[] args) {
-	 
+	    int k=0;
 		String encodeName=PasswordUitl.passwordeEncoded("Ajay");
 		System.out.println("Endocoded name "+encodeName);
 		String decodeName=PasswordUitl.passworddecoded(encodeName);
